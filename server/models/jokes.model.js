aba99b31fee03c8457e3e579447b3d2aef0dfeb3
weicheng lee <z82111712@gmail.com>
2022-01-22 17:53:05 -0800

    validation for min length for both setup and punchline also required for both

--- server/models/jokes.model.js
+++ server/models/jokes.model.js
@@ -3,8 +3,17 @@ const mongoose = require("mongoose")
 
 // create the joke schema (object model)
 const JokeSchema = new mongoose.Schema({
-    setup: String,
-    punchline: String,
+    setup: {
+        type: String,
+        required: [true, "setup required"],
+        minLength: [10, "setup at least 10 characters"]
+    }
+    ,
+    punchline: {
+        type: String,
+        required: [true, "setup required"],
+        minLength: [3, "setup at least 3 characters"]
+    }
 }, { timestamps: true })
 
 // the Class we making our new object
